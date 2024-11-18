507513463655e106111a7dd90cbffa210110c625
Александр Зиновьев <Sashazin2006@mail.ru>
2024-11-18 05:00:52 +0300

    meow

---
+++ hw17nov/solar_physics.py
@@ -0,0 +1,152 @@
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+class Star():
+    """Тип данных, описывающий звезду.
+    Содержит массу, координаты, скорость звезды,
+    а также визуальный радиус звезды в пикселах и её цвет.
+    """
+
+    type = "star"
+    """Признак объекта звезды"""
+
+    m = 0
+    """Масса звезды"""
+
+    x = 0
+    """Координата по оси **x**"""
+
+    y = 0
+    """Координата по оси **y**"""
+
+    Vx = 0
+    """Скорость по оси **x**"""
+
+    Vy = 0
+    """Скорость по оси **y**"""
+
+    Fx = 0
+    """Сила по оси **x**"""
+
+    Fy = 0
+    """Сила по оси **y**"""
+
+    R = 5
+    """Радиус звезды"""
+
+    color = "red"
+    """Цвет звезды"""
+
+    image = None
+    """Изображение звезды"""
+
+
+class Planet():
+    """Тип данных, описывающий планету.
+    Содержит массу, координаты, скорость планеты,
+    а также визуальный радиус планеты в пикселах и её цвет
+    """
+
+    type = "planet"
+    """Признак объекта планеты"""
+
+    m = 0
+    """Масса планеты"""
+
+    x = 0
+    """Координата по оси **x**"""
+
+    y = 0
+    """Координата по оси **y**"""
+
+    Vx = 0
+    """Скорость по оси **x**"""
+
+    Vy = 0
+    """Скорость по оси **y**"""
+
+    Fx = 0
+    """Сила по оси **x**"""
+
+    Fy = 0
+    """Сила по оси **y**"""
+
+    R = 5
+    """Радиус планеты"""
+
+    color = "green"
+    """Цвет планеты"""
+
+    image = None
+    """Изображение планеты"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue
+
+        rx = obj.x - body.x
+        ry = obj.y - body.y
+        r = (rx**2 + ry**2) ** 0.5
+        if rx != 0 and ry != 0 and r != 0:
+            body.Fx += gravitational_constant * body.m * obj.m / rx**2
+            body.Fy += gravitational_constant * body.m * obj.m / ry**2
+
+
+def move_space_object(body, dt):
+    """Перемещает тело в соответствии с действующей на него силой.
+
+    Параметры:
+
+    **body** — тело, которое нужно переместить.
+    """
+
+    ax = body.Fx / body.m
+    body.x += body.Vx * dt
+    body.Vx += ax * dt
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt
+    body.Vy += ay * dt
+
+def calculate_w(body, space_objects):
+    for obj in space_objects:
+        if body == obj:
+            continue
+
+        rx = obj.x - body.x
+        ry = obj.y - body.y
+        r = (rx**2 + ry**2) ** 0.5
+        v = (body.vx**2 + body.vy**2) ** 0.5
+        if r != 0:
+            w = v / r
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+
+    **dt** — шаг по времени
+    """
+
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for body in space_objects:
+        move_space_object(body, dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
